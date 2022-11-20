9475bcdb67871979b2bf7330403cf0403469694f
MFdesigns <michel.faeh@stafag.ch>
2022-11-20 17:24:16 +0100

    Supported leaf 0h

--- cli.c
+++ cli.c
@@ -4,9 +4,14 @@
 int main() {
     printf("CPUID Information\n");
 
-    bool hasCpuid = getCpuidInfo();
+    struct X64Info cpuid = {};
+    bool hasCpuid = getCpuidInfo(&cpuid);
     if (!hasCpuid) {
         printf("This CPU does not support CPUID\n");
         return 1;
     }
+
+    printf("Leaf 0H:\n");
+    printf("\tMaximum Input Value for Basic CPUID Information: 0x%x\n", cpuid.maxInputBasicInfo);
+    printf("\tVendor: %s\n", cpuid.vendorString);
 }
--- cpuid.c
+++ cpuid.c
@@ -1,6 +1,18 @@
 #include <stdbool.h>
+#include <stdint.h>
 
-bool getCpuidInfo() {
+#define CPUID_VENDOR_STRING_SIZE 12
+
+struct X64Info {
+    uint32_t maxInputBasicInfo;
+    uint32_t vendorEbx;
+    uint32_t vendorEcx;
+    uint32_t vendorEdx;
+
+    char vendorString[CPUID_VENDOR_STRING_SIZE + 1];
+};
+
+bool getCpuidInfo(struct X64Info* cpuid) {
     // Check if cpu supports the CPUID instruction by changing the id flag
     // in the FLAGS register and see if that worked
     bool supportsCpuid = false;
@@ -22,6 +34,30 @@ bool getCpuidInfo() {
     );
     if (!supportsCpuid) {
         return false;
+    }
+
+    // -------------------------------------------------
+    //                      Leaf 0
+    // -------------------------------------------------
+
+    { 
+        uint32_t leaf = 0;
+        asm(
+            "mov eax, %4                \n"
+            "cpuid                      \n"
+            "mov %0, eax                \n"
+            "mov %1, ebx                \n"
+            "mov %2, ecx                \n"
+            "mov %3, edx                \n"
+            : "=m" (cpuid->maxInputBasicInfo), "=m" (cpuid->vendorEbx), "=m" (cpuid->vendorEcx), "=m" (cpuid->vendorEdx)
+            : "m" (leaf)
+            : "eax", "ebx", "ecx", "edx"
+        );
+
+        // Convert vendor id in registers to string
+        *(uint32_t*)cpuid->vendorString     = cpuid->vendorEbx;
+        *(uint32_t*)&cpuid->vendorString[4] = cpuid->vendorEdx;
+        *(uint32_t*)&cpuid->vendorString[8] = cpuid->vendorEcx;
     }
 
     return true;
